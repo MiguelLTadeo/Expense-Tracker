109090abaea196df25c3e6a7b06c3321cb7ddf31
MiguelLTadeo <migueltadeo.aluno@unipampa.edu.br>
2026-03-16 11:57:16 -0300

    rate limit

--- app/internal/routes/expensesRoutes.go
+++ app/internal/routes/expensesRoutes.go
@@ -4,13 +4,17 @@ import (
 	"net/http"
 
 	"github.com/MiguelLTadeo/Expense-Tracker.git/internal/service/controllers"
+	"github.com/didip/tollbooth"
 	"gorm.io/gorm"
 )
 
 func ExpenseRoutes(db *gorm.DB) {
-	http.HandleFunc("/expense/create", controllers.CreateExpenseHandler(db))
-	http.HandleFunc("/expense/delete", controllers.DeleteExpenseHandler(db))
-	http.HandleFunc("/expense/get", controllers.GetExpenseHandler(db))
-	http.HandleFunc("/expense/list", controllers.ListExpenseHandler(db))
+	limiter := tollbooth.NewLimiter(5, nil) // 5 req/s
+	limiter.SetIPLookups([]string{"X-Forwarded-For", "RemoteAddr"})
 
+	http.Handle("/expense/create", tollbooth.LimitFuncHandler(limiter, controllers.CreateExpenseHandler(db)))
+	http.Handle("/expense/delete", tollbooth.LimitFuncHandler(limiter, controllers.DeleteExpenseHandler(db)))
+	http.Handle("/expense/get", tollbooth.LimitFuncHandler(limiter, controllers.GetExpenseHandler(db)))
+	http.Handle("/expense/list", tollbooth.LimitFuncHandler(limiter, controllers.ListExpenseHandler(db)))
+	http.Handle("/expense/update", tollbooth.LimitFuncHandler(limiter, controllers.UpdateExpenseHandler(db)))
 }
