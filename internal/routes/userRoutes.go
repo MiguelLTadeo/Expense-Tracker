7ce8fd91d26d0e8e3401a763dd73319a4eb8a8c2
MiguelLTadeo <migueltadeo.aluno@unipampa.edu.br>
2026-03-19 10:30:56 -0300

    hello route

--- internal/routes/userRoutes.go
+++ internal/routes/userRoutes.go
@@ -1,6 +1,8 @@
 package routes
 
 import (
+	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/MiguelLTadeo/Expense-Tracker.git/internal/service/controllers"
@@ -12,8 +14,15 @@ func UserRoutes(db *gorm.DB) {
 	limiter := tollbooth.NewLimiter(5, nil) // 5 req/s
 	limiter.SetIPLookups([]string{"X-Forwarded-For", "RemoteAddr"})
 
+	http.Handle("/hello", tollbooth.LimitFuncHandler(limiter, getHello))
+
 	http.Handle("/user/create", tollbooth.LimitFuncHandler(limiter, controllers.CreateUserHandler(db)))
 	http.Handle("/user/delete", tollbooth.LimitFuncHandler(limiter, controllers.DeleteUserHandler(db)))
 	http.Handle("/user/login", tollbooth.LimitFuncHandler(limiter, controllers.LoginUserhandler(db)))
 	http.Handle("/user/update", tollbooth.LimitFuncHandler(limiter, controllers.UpdateUserHandler(db)))
+}
+
+func getHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got /hello request\n")
+	io.WriteString(w, "Hello, HTTP!\n")
 }
